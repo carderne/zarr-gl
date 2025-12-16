3ed650865c8b1de9b7f8ab6351aa13a2996d79ac
james-willis <james-willis@users.noreply.github.com>
2025-12-16 00:31:51 +0000

    deploy: 7d76e2963970d2b356274a0175b55eaa98e87b61

---
+++ pr-25/store.d.ts
@@ -0,0 +1,24 @@
+import type { Loader } from "zarr-js";
+export interface RequestParameters {
+    url: string;
+    headers?: {
+        [key: string]: string;
+    };
+    credentials?: RequestCredentials;
+}
+declare const loadZarrV2: (source: string, variable: string, transformRequest?: (url: string) => RequestParameters | Promise<RequestParameters>) => Promise<{
+    loaders: Record<string, Loader>;
+    dimensions: string[];
+    dimArrs: {
+        [k: string]: number[];
+    };
+    levels: number[];
+    maxZoom: number;
+    tileSize: number;
+    crs: string;
+    shape: number[];
+    chunks: number[];
+    fillValue: number;
+}>;
+declare const loadZarr: (source: string, variable: string, version: "v2" | "v3", transformRequest?: (url: string) => RequestParameters | Promise<RequestParameters>) => ReturnType<typeof loadZarrV2>;
+export default loadZarr;
